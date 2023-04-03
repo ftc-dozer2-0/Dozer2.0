6a460b4e563346b5ffbdca0080179661ce0ae10c
Stephan <skruglov.2023@carmelhs.org>
2023-04-03 18:24:36 -0500

    teams.py is being a giant pain, migrated db but something else is causing major headache issues

--- cogs/teams.py
+++ cogs/teams.py
@@ -6,9 +6,9 @@ import discord
 import re
 from discord.ext.commands import BadArgument, guild_only
 
+import db
+from context import DozerContext
 from ._utils import *
-from asyncdb.orm import orm
-from asyncdb import psqlt
 
 
 # alter table team_numbers alter column team_number type text
@@ -46,18 +46,18 @@ class Teams(Cog):
 
     @commands.hybrid_command()
     @guild_only()
-    @app_commands.describe(team_type="ftc, frc, or fll", team_number="Team number")
-    async def setteam(self, ctx, team_type, team_number):
+    @app_commands.describe(team_type = "ftc, frc, fll, vex, vexu, vrc", team_number = "Team number")
+    async def setteam(self, ctx: DozerContext, team_type: str, team_number: str):
         """Sets an association with your team in the database."""
         team_type, team_number = self.validate(team_type, team_number)
 
-        dbcheck = await TeamNumbers.select_one(user_id = ctx.author.id, team_number = team_number,
-                                               team_type = team_type)
+        dbcheck = await TeamNumbers.get_by(user_id = ctx.author.id, team_number = team_number,
+                                           team_type = team_type)
         if dbcheck is None:
             dbtransaction = TeamNumbers(user_id = ctx.author.id, team_number = team_number, team_type = team_type)
-            await dbtransaction.insert()
+            await dbtransaction.update_or_add()
             await ctx.send("Team number set! Note that unlike FRC Dozer, this will not affect your nickname "
-                           "when joining other servers.")
+                           "when joining other servers.", ephemeral = True)
         else:
             raise BadArgument("You are already associated with that team!")
 
@@ -67,15 +67,14 @@ class Teams(Cog):
 
     @commands.hybrid_command()
     @guild_only()
-    @app_commands.describe(team_type="ftc, frc, or fll", team_number="Team number")
-    async def removeteam(self, ctx, team_type, team_number):
+    @app_commands.describe(team_type = "ftc, frc, fll, vex, vexu, vrc", team_number = "Team number")
+    async def removeteam(self, ctx: DozerContext, team_type: str, team_number: str):
         """Removes an association with a team in the database."""
         team_type, team_number = self.validate(team_type, team_number)
-        results = await TeamNumbers.select_one(user_id = ctx.author.id, team_number = team_number,
-                                               team_type = team_type)
+        results = await TeamNumbers.get_by(user_id = ctx.author.id, team_number = team_number, team_type = team_type)
         if results is not None:
-            await results.delete()
-            await ctx.send("Removed association with {} team {}".format(team_type, team_number))
+            await TeamNumbers.delete(user_id = ctx.author.id, team_number = team_number, team_type = team_type)
+            await ctx.send(f"Removed association with {team_type} team {team_number}", ephemeral = True)
         if results is None:
             await ctx.send("Couldn't find any associations with that team!")
 
@@ -86,12 +85,12 @@ class Teams(Cog):
     @commands.hybrid_command()
     @guild_only()
     @app_commands.describe(user = "User to get teams for")
-    async def teamsfor(self, ctx, user: discord.Member = None):
+    async def teamsfor(self, ctx: DozerContext, user: discord.Member = None):
         """Allows you to see the teams for the mentioned user, or yourself if no user is mentioned."""
         if user is None:
             user = ctx.author
 
-        teams = await TeamNumbers.select(user_id = user.id)
+        teams = await TeamNumbers.get_by(user_id = user.id)
         if not teams:
             raise BadArgument("Couldn't find any team associations for that user!")
         else:
@@ -99,7 +98,7 @@ class Teams(Cog):
             e.title = 'Teams for {}'.format(user.display_name)
             e.description = "Teams: \n"
             for i in teams:
-                e.description = "{} {} Team {} \n".format(e.description, i.team_type.upper(), i.team_number)
+                e.description = f"{e.description} {i.team_type.upper()} Team {i.team_number} \n"
             if len(e.description) > 4000:
                 e.description = e.description[:4000] + "..."
             await ctx.send(embed = e)
@@ -110,11 +109,11 @@ class Teams(Cog):
 
     @commands.hybrid_group(invoke_without_command = True)
     @guild_only()
-    @app_commands.describe(team_type="ftc, frc, or fll", team_number="Team number")
+    @app_commands.describe(team_type = "ftc, frc, or fll", team_number = "Team number")
     async def onteam(self, ctx, team_type, team_number):
         """Allows you to see who has associated themselves with a particular team."""
         team_type, team_number = self.validate(team_type, team_number)
-        users = await TeamNumbers.select(team_number = team_number, team_type = team_type)
+        users = await TeamNumbers.get_by(team_number = team_number, team_type = team_type)
 
         user_ids = [user.user_id for user in users]
 
@@ -153,18 +152,12 @@ class Teams(Cog):
     @guild_only()
     async def top(self, ctx):
         """Show the top 10 teams by number of members in this guild."""
-
-        # adapted from the FRC Dozer's equivalent.
-        query = f"""SELECT team_type, team_number, count(*)
-                FROM {TeamNumbers.table_name()}
-                WHERE user_id = ANY($1) --first param: list of user IDs
-                GROUP BY team_type, team_number
-                ORDER BY count DESC, team_type, team_number
-                LIMIT 10"""
-
-        async with orm.acquire() as conn:
-            counts = await conn.fetch(query, [member.id for member in ctx.guild.members])
-
+        users = [mem.id for mem in ctx.guild.members]
+        counts = await TeamNumbers.top10(users)
+        embed = discord.Embed(title = f'Top teams in {ctx.guild.name}', color = discord.Color.blue())
+        embed.description = '\n'.join(
+            f'{type_.upper()} team {num} ({count} member{"s" if count > 1 else ""})' for (type_, num, count) in counts)
+        await ctx.send(embed = embed)
         embed = discord.Embed(title = f'Top teams in {ctx.guild.name}', color = discord.Color.blue())
         embed.description = '\n'.join(
             f'{ent["team_type"].upper()} team {ent["team_number"]} '
@@ -176,13 +169,70 @@ class Teams(Cog):
     """
 
 
-class TeamNumbers(orm.Model):
-    """DB object for tracking team associations."""
+class TeamNumbers(db.DatabaseTable):
+    """Database operations for tracking team associations."""
     __tablename__ = 'team_numbers'
-    __primary_key__ = ("user_id", "team_number", "team_type")
-    user_id: psqlt.bigint
-    team_number: psqlt.text
-    team_type: psqlt.text
+    __uniques__ = ('user_id', 'team_number', 'team_type',)
+
+    @classmethod
+    async def initial_create(cls):
+        """Create the table in the database"""
+        async with db.Pool.acquire() as conn:
+            await conn.execute(f"""
+            CREATE TABLE {cls.__tablename__} (
+            user_id bigint NOT NULL,
+            team_number text NOT NULL,
+            team_type VARCHAR NOT NULL,
+            PRIMARY KEY (user_id, team_number, team_type)
+            )""")
+
+    def __init__(self, user_id, team_number, team_type):
+        super().__init__()
+        self.user_id = user_id
+        self.team_number = team_number
+        self.team_type = team_type
+
+    async def update_or_add(self):
+        """Assign the attribute to this object, then call this method to either insert the object if it doesn't exist in
+        the DB or update it if it does exist. It will update every column not specified in __uniques__."""
+        # This is its own functions because all columns must be unique, which breaks the syntax of the other one
+        keys = []
+        values = []
+        for var, value in self.__dict__.items():
+            # Done so that the two are guaranteed to be in the same order, which isn't true of keys() and values()
+            if value is not None:
+                keys.append(var)
+                values.append(value)
+        async with db.Pool.acquire() as conn:
+            statement = f"""
+            INSERT INTO {self.__tablename__} ({", ".join(keys)})
+            VALUES({','.join(f'${i + 1}' for i in range(len(values)))}) 
+            """
+            await conn.execute(statement, *values)
+
+    @classmethod
+    async def get_by(cls, **kwargs):
+        results = await super().get_by(**kwargs)
+        result_list = []
+        for result in results:
+            obj = TeamNumbers(user_id = result.get("user_id"),
+                              team_number = result.get("team_number"),
+                              team_type = result.get("team_type"))
+            result_list.append(obj)
+        return result_list
+
+    # noinspection SqlResolve
+    @classmethod
+    async def top10(cls, user_ids):
+        """Returns the top 10 team entries"""
+        query = f"""SELECT team_type, team_number, count(*)
+                FROM {cls.__tablename__}
+                WHERE user_id = ANY($1) --first param: list of user IDs
+                GROUP BY team_type, team_number
+                ORDER BY count DESC, team_type, team_number
+                LIMIT 10"""
+        async with db.Pool.acquire() as conn:
+            return await conn.fetch(query, user_ids)
 
 
 async def setup(bot):
